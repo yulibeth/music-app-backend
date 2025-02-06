3aa86e298ab318d14cbd137470201f73d32c6162
yulibeth <yulibeth9@gmail.com>
2025-02-06 18:36:15 -0500

    Initial commit: Project setup with Spring Boot, JPA, H2, and Security

---
+++ src/main/java/com/quipux/music_app/MusicAppApplication.java
@@ -0,0 +1,13 @@
+package com.quipux.music_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicAppApplication.class, args);
+	}
+
+}
